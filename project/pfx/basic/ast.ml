bdfb94e8904ea2b27ca889c1b0772b0574d12209
MariemMARZOUGUI <127297843+MariemMARZOUGUI@users.noreply.github.com>
2024-03-12 14:45:09 +0100

    question 4.1

--- project/pfx/basic/ast.ml
+++ project/pfx/basic/ast.ml
@@ -1,12 +1,22 @@
 type command =
-  DefineMe (* Question 4.1 *)
+| Push of int      (* Push an integer onto the stack *)
+| Add              (* Pop two values from the stack, add them, and push the result *)
+| Sub              (* Pop two values from the stack, subtract the second from the first, and push the result *)
+| Mul              (* Pop two values from the stack, multiply them, and push the result *)
+| Div              (* Pop two values from the stack, divide the first by the second, and push the result *)
+| Swap             (* Swap the top two values on the stack *)
+| Pop              (* Pop the top value from the stack *)
 
 type program = int * command list
 
-(* add here all useful functions and types  related to the AST: for instance  string_of_ functions *)
-
 let string_of_command = function
-  | DefineMe -> "to be done"
+  | Push n -> "Push " ^ string_of_int n
+  | Add -> "Add"
+  | Sub -> "Sub"
+  | Mul -> "Mul"
+  | Div -> "Div"
+  | Swap -> "Swap"
+  | Pop -> "Pop"
 
 let string_of_commands cmds = String.concat " " (List.map string_of_command cmds)
 
