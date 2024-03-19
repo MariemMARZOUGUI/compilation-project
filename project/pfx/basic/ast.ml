c39d5b4ef3c6bf43fb73e7b4959797e19488fb64
MariemMARZOUGUI <mariemmarzougui46@gmail.com>
2024-03-19 16:20:06 +0100

    ADDED MAIN

--- project/pfx/basic/ast.ml
+++ project/pfx/basic/ast.ml
@@ -1,24 +1,24 @@
 type command =
-| Push of int      (* Push an integer onto the stack *)
-| Add              (* Pop two values from the stack, add them, and push the result *)
-| Sub              (* Pop two values from the stack, subtract the second from the first, and push the result *)
-| Mult             (* Pop two values from the stack, multiply them, and push the result *)
-| Div              (* Pop two values from the stack, divide the first by the second, and push the result *)
-| Rem              (* Pop two values from the stack, calculate the modulo of the first divided by the second, and push the result *)
-| Swap             (* Swap the top two values on the stack *)
-| Pop              (* Pop the top value from the stack *)
+| PUSH of int      (* Push an integer onto the stack *)
+| ADD              (* Pop two values from the stack, add them, and push the result *)
+| SUB              (* Pop two values from the stack, subtract the second from the first, and push the result *)
+| MULT             (* Pop two values from the stack, multiply them, and push the result *)
+| DIV              (* Pop two values from the stack, divide the first by the second, and push the result *)
+| REM              (* Pop two values from the stack, calculate the modulo of the first divided by the second, and push the result *)
+| SWAP             (* Swap the top two values on the stack *)
+| POP              (* Pop the top value from the stack *)
 
 type program = int * command list
 
 let string_of_command = function
-  | Push n -> "Push " ^ string_of_int n
-  | Add -> "Add"
-  | Sub -> "Sub"
-  | Mult -> "Mult"
-  | Div -> "Div"
-  | Rem -> "Rem"
-  | Swap -> "Swap"
-  | Pop -> "Pop"
+  | PUSH n -> "PUSH" ^ string_of_int n
+  | ADD -> "ADD"
+  | SUB -> "SUB"
+  | MULT -> "MULT"
+  | DIV -> "DIV"
+  | REM -> "REM"
+  | SWAP -> "SWAP"
+  | POP -> "POP"
 
 let string_of_commands cmds = String.concat " " (List.map string_of_command cmds)
 
--- project/pfx/basic/ast.mli
+++ project/pfx/basic/ast.mli
@@ -1,13 +1,13 @@
 (* The type of the commands for the stack machine *)
 type command =
-| Push of int      (* Push an integer onto the stack *)
-| Add              (* Pop two values from the stack, add them, and push the result *)
-| Sub              (* Pop two values from the stack, subtract the second from the first, and push the result *)
-| Mult              (* Pop two values from the stack, multiply them, and push the result *)
-| Div              (* Pop two values from the stack, divide the first by the second, and push the result *)
-| Rem              (* Pop two values from the stack, calculate the modulo of the first divided by the second, and push the result *)
-| Swap             (* Swap the top two values on the stack *)
-| Pop              (* Pop the top value from the stack *)
+| PUSH of int      (* Push an integer onto the stack *)
+| ADD              (* Pop two values from the stack, add them, and push the result *)
+| SUB             (* Pop two values from the stack, subtract the second from the first, and push the result *)
+| MULT             (* Pop two values from the stack, multiply them, and push the result *)
+| DIV             (* Pop two values from the stack, divide the first by the second, and push the result *)
+| REM             (* Pop two values from the stack, calculate the modulo of the first divided by the second, and push the result *)
+| SWAP            (* Swap the top two values on the stack *)
+| POP              (* Pop the top value from the stack *)
 
 (* The type for programs *)
 type program = int * command list
