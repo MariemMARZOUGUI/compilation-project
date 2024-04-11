325ec04b7af9b476a977fe77a6a8c4a42f23ccba
MariemMARZOUGUI <127297843+MariemMARZOUGUI@users.noreply.github.com>
2024-04-12 01:10:11 +0200

    trying ex9

--- project/pfx/fun/ast.ml
+++ project/pfx/fun/ast.ml
@@ -1,20 +1,18 @@
 type command =
-| PUSH of int      (* Push an integer onto the stack *)
-| ADD              (* Pop two values from the stack, add them, and push the result *)
-| SUB              (* Pop two values from the stack, subtract the second from the first, and push the result *)
-| MULT             (* Pop two values from the stack, multiply them, and push the result *)
-| DIV              (* Pop two values from the stack, divide the first by the second, and push the result *)
-| REM              (* Pop two values from the stack, calculate the modulo of the first divided by the second, and push the result *)
-| SWAP             (* Swap the top two values on the stack *)
-| POP              (* Pop the top value from the stack *)
- (* extending the Pfx langugae to support functions and applications *)  
-| Q of command list
-| EXEC
-| GET
+  | PUSH of int      (* Push an integer onto the stack *)
+  | ADD              (* Pop two values from the stack, add them, and push the result *)
+  | SUB              (* Pop two values from the stack, subtract the second from the first, and push the result *)
+  | MULT             (* Pop two values from the stack, multiply them, and push the result *)
+  | DIV              (* Pop two values from the stack, divide the first by the second, and push the result *)
+  | REM              (* Pop two values from the stack, calculate the modulo of the first divided by the second, and push the result *)
+  | SWAP             (* Swap the top two values on the stack *)
+  | POP              (* Pop the top value from the stack *)
+ (* extending the Pfx language to support functions and applications *)  
+  | Q of command list
+  | EXEC             (* Execute a sequence of commands *)
+  | GET              (* Get a value from the stack *)
 
-type program = int * command list
-
-let string_of_command = function
+let rec string_of_command = function
   | PUSH n -> "PUSH" ^ string_of_int n
   | ADD -> "ADD"
   | SUB -> "SUB"
@@ -28,7 +26,8 @@ let string_of_command = function
   | EXEC -> "EXEC"
   | GET -> "GET"
 
-let string_of_commands cmds = String.concat " " (List.map string_of_command cmds)
+and string_of_commands cmds = String.concat " " (List.map string_of_command cmds)
 
 let string_of_program (args, cmds) = Printf.sprintf "%i args: %s\n" args (string_of_commands cmds)
+
 
