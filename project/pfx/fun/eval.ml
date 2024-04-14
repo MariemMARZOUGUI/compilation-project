ee563e63fe82225bf66fb1f1dc9d9124674650c8
MariemMARZOUGUI <mariemmarzougui46@gmail.com>
2024-04-14 15:49:48 +0200

    exo 7

--- project/pfx/fun/eval.ml
+++ project/pfx/fun/eval.ml
@@ -11,61 +11,32 @@ let string_of_state (cmds, stack) =
    | cmd::_ -> sprintf "executing %s" (string_of_command cmd))^
     (sprintf " with stack %s" (String.concat ";" (List.map (fun x -> match x with | Int i -> string_of_int i | Executable _ -> "Executable") stack)))
 
+
 (* Question 4.2 *)
 let step state =
   match state with
-  
   | [], _ -> Error("Nothing to step",state)
-
+  (* Valid configurations *)
   | PUSH(n) :: q, stack -> Ok (q, Int n :: stack)
-
-  | POP :: q, stack -> 
-    ( match stack with 
-      | _ :: s -> Ok (q, s)
-      | _ -> Error("Empty stack", ([], stack))
-    ) 
-
-  | SWAP :: q, stack -> 
-    ( match stack with 
-      | v1:: v2 :: s -> Ok (q, v2 :: v1 :: s)
-      | _ -> Error("Empty stack or no enough elements", ([], stack))
-    ) 
-  
-  | ADD :: q, stack ->
-    ( match stack with 
-      | v1 :: v2 :: s -> Ok (q, (v1+v2) :: s)
-      | _ -> Error("Not enough arguments for addition", ([], stack))
-    )
-
-  | SUB ::q , stack ->
-    ( match stack with
-      | v1 :: v2 ::s -> Ok (q, (v1-v2)::s)
-      | _ -> Error("Not enough arguments for substraction", ([], stack))
-    )
-  
-  | DIV ::q , stack ->
-    ( match stack with
-      | v1 :: v2 ::s -> Ok (q, (v1/v2)::s)
-      | _ -> Error("Not enough arguments for division", ([], stack))
-    )
-  
-  | MULT ::q , stack ->
-    ( match stack with
-      | v1 :: v2 ::s -> Ok (q, (v1*v2)::s)
-      | _ -> Error("Not enough arguments for multiplication", ([], stack))
-    )
-
-  | REM :: q, stack -> 
-    ( match stack with
-      | v1 :: v2 :: s -> Ok (q, (v1 mod v2) :: s)
-      | _ -> Error("Not enough arguments for mod", ([], stack))
-    )
-
-  (* New commands*)
+  | POP :: q, _ :: s -> Ok (q, s)
+  | POP :: q, [] -> Error("Runtime error: Stack underflow with Pop", (q, []))
+  | SWAP :: q, n1 :: n2 :: s -> Ok (q, n2 :: n1 :: s)
+  | SWAP :: q, _ -> Error("Runtime error: Not enough elements for Swap", (q, []))
+  | ADD :: q, Int n1 :: Int n2 :: s -> Ok (q, Int (n2 + n1) :: s)
+  | ADD :: q, _ -> Error("Runtime error: Not enough elements for Add", (q, []))
+  | SUB :: q, Int n1 :: Int n2 :: s -> Ok (q, Int (n1 - n2) :: s)
+  | SUB :: q, _ -> Error("Runtime error: Not enough elements for Sub", (q, []))
+  | MULT :: q, Int n1 :: Int n2 :: s -> Ok (q, Int (n2 * n1) :: s)
+  | MULT :: q, _ -> Error("Runtime error: Not enough elements for Mul", (q, []))
+  | DIV :: q,Int n1 :: Int n2 :: s when n2 != 0 -> Ok (q, Int (n1 / n2) :: s)
+  | DIV :: q, _ :: Int 0 :: _ -> Error("Runtime error: Division by zero", (q, []))
+  | DIV :: q, _ -> Error("Runtime error: Not enough elements for Div", (q, []))
+  | REM :: q, Int n1 :: Int n2 :: s when n2 != 0 -> Ok (q, Int (n1 mod n2) :: s)
+  | REM :: q, _ :: Int 0 :: _ -> Error("Runtime error: Division by zero in Rem", (q, []))
+  | REM :: q, _ -> Error("Runtime error: Not enough elements for Rem", (q, []))
+  (*Added code *)
   | DoExec(instruc) :: q, stack -> Ok(q, Executable instruc :: stack)
-  
   | EXEC ::q, Executable instruc :: stackTail -> Ok(instruc@q,stackTail)
-  
   | GET :: q, Int n :: stackTail ->
     let rec getNth i = function
       |Int t::q1 -> if i = 0 then t else getNth (i-1) q1 
@@ -95,7 +66,7 @@ let eval_program (numargs, cmds) args =
        end
   in
   if numargs = List.length args then
-    match execute (cmds, List.map(fun x -> Int x) args)  with
+    match execute (cmds, List.map(fun x -> Int x) args) with
     | Ok None -> printf "No result\n"
     | Ok(Some result) -> printf "= %i\n" result
     | Error(msg,s) -> printf "Raised error %s in state %s\n" msg (string_of_state s)
